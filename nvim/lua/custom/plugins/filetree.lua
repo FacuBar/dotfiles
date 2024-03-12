ad0755fec88a44e73ba906ea894d3bd7690a04f3
FacuBar <efacubar@gmail.com>
2024-03-11 23:22:50 -0300

    update config

--- nvim/lua/custom/plugins/filetree.lua
+++ nvim/lua/custom/plugins/filetree.lua
@@ -1,27 +1,28 @@
+vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
   },
-  config = function ()
-    vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+  config = function()
+    vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
     require('neo-tree').setup {
       filesystem = {
         filtered_items = {
-          visible = "hide",
+          visible = 'hide',
           hide_dotfiles = false,
           hide_gitignored = true,
-        }
+        },
       },
       enable_git_status = true,
       enable_diagnostics = true,
       window = {
-        position = "right",
+        position = 'right',
       },
-      vim.keymap.set('n', '<leader>ft', '<Cmd>Neotree toggle<Cr>', { desc = '[F]ile [T]oggle' })
+      vim.keymap.set('n', '<leader>ft', '<Cmd>Neotree toggle<Cr>', { desc = '[F]ile [T]oggle' }),
     }
   end,
 }
